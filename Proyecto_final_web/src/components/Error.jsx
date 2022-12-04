8384671dff38d94edc3d47512749c5e0f365897d
camilotenorio1234 <99371279+camilotenorio1234@users.noreply.github.com>
2022-12-04 03:27:28 -0500

    Proyecto_final_web

---
+++ Proyecto_final_web/src/components/Error.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Error = () => (
+  <div className="w-full flex justify-center items-center">
+    <h1 className="font-bold text-2xl text-white">Te has perdido, regresaaaa.</h1>
+  </div>
+);
+
+export default Error;
---
+++ Proyecto_final_web/src/components/Sidebar.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
+import { HiOutlineHashtag, HiOutlineHome, HiOutlineMenu, HiOutlinePhotograph, HiOutlineUserGroup } from 'react-icons/hi';
+import { RiCloseLine } from 'react-icons/ri';
+
+import { logo } from '../assets';
+
+const links = [
+  { name: 'Descubrete', to: '/', icon: HiOutlineHome },
+  { name: 'Cerca de ti', to: '/around-you', icon: HiOutlinePhotograph },
+  { name: 'Los mas GOD:', to: '/top-artists', icon: HiOutlineUserGroup },
+  { name: 'Temazos top:', to: '/top-charts', icon: HiOutlineHashtag },
+];
+
+const NavLinks = ({ handleClick }) => (
+  <div className="mt-10">
+    {links.map((item) => (
+      <NavLink
+        key={item.name}
+        to={item.to}
+        className="flex flex-row justify-start items-center my-8 text-sm font-medium font-bold text-black hover:text-cyan"
+        onClick={() => handleClick && handleClick()}
+      >
+        <item.icon className="w-6 h-6 mr-2" />
+        {item.name}
+      </NavLink>
+    ))}
+  </div>
+);
+
+const Sidebar = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+
+  return (
+    <>
+      <div className="md:flex hidden flex-col w-[240px] py-10 px-4 bg-gradient-to-br from-[#0A7163] to-[#D2691E]">
+        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <NavLinks />
+      </div>
+
+      {/* Mobile sidebar */}
+      <div className="absolute md:hidden block top-6 right-3">
+        {!mobileMenuOpen ? (
+          <HiOutlineMenu className="w-6 h-6 mr-2 text-black" onClick={() => setMobileMenuOpen(true)} />
+        ) : (
+          <RiCloseLine className="w-6 h-6 mr-2 text-black" onClick={() => setMobileMenuOpen(false)} />
+        )}
+      </div>
+
+      <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483D8B] backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
+        <img src={logo} alt="logo" className="w-full h-14 object-contain" />
+        <NavLinks handleClick={() => setMobileMenuOpen(false)} />
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
